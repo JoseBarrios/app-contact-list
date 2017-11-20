ea7f28a938049c6bc7e174560fa9d6e1cd391234
Jose Barrios <jose@cognilab.com>
2017-11-20 14:55:17 -0800

    Fixed issue with Pug rendering on mobile (tab/space mixing). Removed console logs

--- routes/contacts.js
+++ routes/contacts.js
@@ -11,7 +11,7 @@ const COLLECTION = 'people';
 //
 ////////////////////////////////////
 router.param('personID', function (req, res, next, personID) {
-  let _id = dbController.ObjectID(personID);
+	let _id = dbController.ObjectID(personID);
 	dbController.getDocumentByID(COLLECTION, _id)
 		.then(person => {
 			if(person._id){ delete person._id; }
@@ -41,29 +41,29 @@ router.param('display', function (req, res, next, display) {
 ////////////////////////////////////
 router.route('/contacts/create/:display?')
 
-	//Client only requests this page if its mobile
-  .get((req, res) =>{
+//Client only requests this page if its mobile
+	.get((req, res) =>{
 		let locals = {};
 		locals.person = {};
 		//Sends mobile-first view
-    res.render("contacts/card", locals);
+		res.render("contacts/card", locals);
 	})
 
 
-	//Create a new contact in the db
-  .post((req, res) =>{
-    let emergencyContact = {};
-    emergencyContact.givenName = req.body.emergencyGivenName;
-    emergencyContact.familyName = req.body.emergencyFamilyName;
-    emergencyContact.telephone = req.body.emergencyTelephone;
+//Create a new contact in the db
+	.post((req, res) =>{
+		let emergencyContact = {};
+		emergencyContact.givenName = req.body.emergencyGivenName;
+		emergencyContact.familyName = req.body.emergencyFamilyName;
+		emergencyContact.telephone = req.body.emergencyTelephone;
 
-    let person = {};
-    person.knows = [];
-    person.givenName = req.body.givenName;
-    person.familyName = req.body.familyName;
-    person.telephone = req.body.telephone;
-    person.email = req.body.email;
-    person.knows.push(emergencyContact);
+		let person = {};
+		person.knows = [];
+		person.givenName = req.body.givenName;
+		person.familyName = req.body.familyName;
+		person.telephone = req.body.telephone;
+		person.email = req.body.email;
+		person.knows.push(emergencyContact);
 
 		person.meta = {};
 		person.meta.createdOn = Date.now();
@@ -81,14 +81,13 @@ router.route('/contacts/create/:display?')
 			person.identifier = _id.toString();
 			dbController.insertOne(COLLECTION, person)
 				.then(person => {
-					console.log(person)
 					let locals = {};
 					locals.person = person;
 					locals.personID = person.identifer;
 					locals.csrfToken = req.csrfToken();
 					req.onMobile?  res.render("contacts/card", locals) : res.redirect('/contacts?select='+person.identifier)
 				})
-				//DB Error
+			//DB Error
 				.catch(error => {
 					//TODO: return error page
 					console.log('500 error: /contacts/create', error)
@@ -109,14 +108,14 @@ router.route('/contacts/create/:display?')
 //
 ////////////////////////////////////
 router.get('/contacts', function(req, res) {
-  //Default values
-  var sortPrimary = 'givenName';
-  let sortSecondary = 'familyName';
+	//Default values
+	var sortPrimary = 'givenName';
+	let sortSecondary = 'familyName';
 	let search = null;
-  //Check query and adjust default values accordingly
-  if(req.query.sort){ sortPrimary = req.query.sort; }
-  if(req.query.search){ search = req.query.search; }
-  sortSecondary = sortPrimary === 'givenName'? 'familyName':'givenName';
+	//Check query and adjust default values accordingly
+	if(req.query.sort){ sortPrimary = req.query.sort; }
+	if(req.query.search){ search = req.query.search; }
+	sortSecondary = sortPrimary === 'givenName'? 'familyName':'givenName';
 
 	dbController.getDocuments(COLLECTION)
 		.then(cursor => {
@@ -133,7 +132,6 @@ router.get('/contacts', function(req, res) {
 					locals.search = search;
 					locals.people = people;
 					locals.person = people[0];
-					console.log(locals)
 					res.render("contacts/", locals);
 				})
 		})
@@ -147,12 +145,12 @@ router.get('/contacts', function(req, res) {
 
 router.route('/contacts/:personID')
 	.get((req, res) =>{
-    let locals = {};
-    locals.personID = req.personID;
-    locals.person = req.person;
-    locals.csrfToken = req.csrfToken();
-    res.render("contacts/card", locals);
-  })
+		let locals = {};
+		locals.personID = req.personID;
+		locals.person = req.person;
+		locals.csrfToken = req.csrfToken();
+		res.render("contacts/card", locals);
+	})
 
 
 /////////////////////////////////////
@@ -161,35 +159,35 @@ router.route('/contacts/:personID')
 //
 ////////////////////////////////////
 router.route('/contacts/:personID/update/:display?')
-  .post((req, res) =>{
-    let person = {};
-    person.knows = req.person.knows || [];
-    person.meta = req.person.meta || {};
-    person.givenName = req.body.givenName;
-    person.familyName = req.body.familyName;
-    person.email = req.body.email;
-    person.telephone = req.body.telephone;
+	.post((req, res) =>{
+		let person = {};
+		person.knows = req.person.knows || [];
+		person.meta = req.person.meta || {};
+		person.givenName = req.body.givenName;
+		person.familyName = req.body.familyName;
+		person.email = req.body.email;
+		person.telephone = req.body.telephone;
 
-    let emergencyContact = {};
-    emergencyContact.givenName = req.body.emergencyGivenName;
-    emergencyContact.familyName = req.body.emergencyFamilyName;
-    emergencyContact.telephone = req.body.emergencyTelephone;
+		let emergencyContact = {};
+		emergencyContact.givenName = req.body.emergencyGivenName;
+		emergencyContact.familyName = req.body.emergencyFamilyName;
+		emergencyContact.telephone = req.body.emergencyTelephone;
 		person.knows.unshift(emergencyContact);
 
 		person.meta = {};
 		person.meta.updatedOn = Date.now();
 
-    let id = dbController.ObjectID(req.personID);
-    dbController.updateDocument(COLLECTION, id, person)
-      .then(result => {
+		let id = dbController.ObjectID(req.personID);
+		dbController.updateDocument(COLLECTION, id, person)
+			.then(result => {
 				req.onMobile?  res.redirect('/contacts/'+req.personID) : res.redirect('/contacts?select='+req.personID)
-      })
+			})
 			.catch(error => {
 				//TODO: return error page
 				console.log('500 error: /contacts/update', error)
 				res.sendStatus(500);
 			})
-  })
+	})
 
 
 /////////////////////////////////////
@@ -198,18 +196,18 @@ router.route('/contacts/:personID/update/:display?')
 //
 ////////////////////////////////////
 router.route('/contacts/:personID/delete')
-  .post((req, res) =>{
-    let id = dbController.ObjectID(req.personID);
-    dbController.deleteDocument(COLLECTION, id)
-      .then(result => {
-        res.redirect('/contacts');
-      })
+	.post((req, res) =>{
+		let id = dbController.ObjectID(req.personID);
+		dbController.deleteDocument(COLLECTION, id)
+			.then(result => {
+				res.redirect('/contacts');
+			})
 			.catch(error => {
 				//TODO: return error page
 				console.log('500 error: /contacts/delete', error)
 				res.sendStatus(500);
 			})
-  })
+	})
 
 
 
